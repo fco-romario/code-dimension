1387cf3be72611cbd1bfc236889e17c4d35c9456
fco-romario <fco.romario.alves@gmail.com>
2025-11-21 22:58:01 -0300

    feat: Cria Enum Transaction-type e Interface Transaction

--- gerenciador-financeiro/src/app/features/home/home.ts
+++ gerenciador-financeiro/src/app/features/home/home.ts
@@ -1,16 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { Balance } from './components/balance/balance';
+import { TransactionItem } from './components/transaction-item/transaction-item';
+import { Transaction } from '../../shared/interfaces/transaction';
+import { TransactionType } from '../../shared/enums/transaction-type';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance],
+  imports: [Balance, TransactionItem],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-  transactions = signal([
-    { value: 100, type: 'income' },
-    { value: 50, type: 'income' },
-    { value: 150, type: 'outcome' },
+  transactions = signal<Transaction[]>([
+    { title: 'Salário', value: 100, type: TransactionType.INCOME },
+    { title: 'VA', value: 50, type: TransactionType.INCOME },
+    { title: 'Aluguel', value: 150, type: TransactionType.OUTCOME },
   ]);
 }
