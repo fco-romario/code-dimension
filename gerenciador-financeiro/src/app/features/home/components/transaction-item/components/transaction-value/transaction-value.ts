04a09bf85f7a509b03a9ae3dc4cd082fe947ab7a
fco-romario <fco.romario.alves@gmail.com>
2025-11-21 23:04:50 -0300

    Cria componente Transaction-value e Transaction-item

---
+++ gerenciador-financeiro/src/app/features/home/components/transaction-item/components/transaction-value/transaction-value.ts
@@ -0,0 +1,22 @@
+import { Component, computed, input } from '@angular/core';
+import { Transaction } from '../../../../../../shared/interfaces/transaction';
+import { TransactionType } from '../../../../../../shared/enums/transaction-type';
+
+const CssClasses = {
+  [TransactionType.INCOME]: 'income',
+  [TransactionType.OUTCOME]: 'outcome',
+}
+@Component({
+  selector: 'app-transaction-value',
+  imports: [],
+  templateUrl: './transaction-value.html',
+  styleUrl: './transaction-value.scss',
+  host: {
+    '[class]': 'cassClass()'
+  }
+})
+export class TransactionValue {
+  transaction = input.required<Transaction>();
+
+  cassClass = computed(() => CssClasses[this.transaction().type])
+}
