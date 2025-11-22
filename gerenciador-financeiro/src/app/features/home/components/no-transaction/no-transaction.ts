961e77dcaabe4f38662b694b8f141a814364bfed
fco-romario <fco.romario.alves@gmail.com>
2025-11-21 23:23:27 -0300

    Adiciona componente para mostrar mensagem quando não há transações

---
+++ gerenciador-financeiro/src/app/features/home/components/no-transaction/no-transaction.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-no-transaction',
+  imports: [MatCardModule],
+  templateUrl: './no-transaction.html',
+  styleUrl: './no-transaction.scss',
+})
+export class NoTransaction {
+
+}
--- gerenciador-financeiro/src/app/features/home/home.ts
+++ gerenciador-financeiro/src/app/features/home/home.ts
@@ -3,17 +3,18 @@ import { Balance } from './components/balance/balance';
 import { TransactionItem } from './components/transaction-item/transaction-item';
 import { Transaction } from '../../shared/interfaces/transaction';
 import { TransactionType } from '../../shared/enums/transaction-type';
+import { NoTransaction } from './components/no-transaction/no-transaction';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, TransactionItem],
+  imports: [Balance, TransactionItem, NoTransaction],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   transactions = signal<Transaction[]>([
-    { title: 'Salário', value: 100, type: TransactionType.INCOME },
-    { title: 'VA', value: 50, type: TransactionType.INCOME },
-    { title: 'Aluguel', value: 150, type: TransactionType.OUTCOME },
+    // { title: 'Salário', value: 100, type: TransactionType.INCOME },
+    // { title: 'VA', value: 50, type: TransactionType.INCOME },
+    // { title: 'Aluguel', value: 150, type: TransactionType.OUTCOME },
   ]);
 }
