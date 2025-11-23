507279e7c7cd542bb6d758d223aedcbf48704d39
fco-romario <fco.romario.alves@gmail.com>
2025-11-22 21:04:06 -0300

    refactor: adiciona pasta pai 'transaction' para a arvore de pasta da transação

--- gerenciador-financeiro/src/app/features/home/components/transaction-item/transaction-item.ts
+++ gerenciador-financeiro/src/app/features/home/components/transaction-item/transaction-item.ts
@@ -1,7 +1,7 @@
 import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { Transaction } from '../../../../shared/interfaces/transaction';
+import { Transaction } from '../../../../shared/transaction/interfaces/transaction';
 import { TransactionValue } from './components/transaction-value/transaction-value';
 
 @Component({
