df94aa161e0e15c950e3ab7785f0ac6c22bde45e
fco-romario <fco.romario.alves@gmail.com>
2025-11-24 21:17:26 -0300

    feat: Adiciona serviço feedback e adiciona snack-bar

---
+++ gerenciador-financeiro/src/app/shared/feedback/services/feedback.service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FeedbackService {
+
+  private _snackBar = inject(MatSnackBar);
+  
+  success(message: string): void {
+    this._snackBar.open(message, 'Ok', {
+      panelClass: 'snak-bar-success-feedback',
+    });
+  }
+}
