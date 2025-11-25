e594532e0747af74cdd93fefbe460fd755fdc806
fco-romario <fco.romario.alves@gmail.com>
2025-11-24 21:18:11 -0300

    feat: Cria formulario para criar transações

--- gerenciador-financeiro/src/app/app.config.ts
+++ gerenciador-financeiro/src/app/app.config.ts
@@ -1,20 +1,29 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(),
     provideEnvironmentNgxMask(
       {
         thousandSeparator:".",
         decimalMarker:",",
       }),
+    {
+      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+      useValue: {
+        horizontalPosition: "center",
+        verticalPosition: "top",
+        duration: 3000
+      } as MatSnackBarConfig
+    } 
   ]
 };
--- gerenciador-financeiro/src/app/features/home/home.ts
+++ gerenciador-financeiro/src/app/features/home/home.ts
@@ -3,11 +3,19 @@ import { Balance } from './components/balance/balance';
 import { TransactionItem } from './components/transaction-item/transaction-item';
 import { Transaction } from '../../shared/transaction/interfaces/transaction';
 import { NoTransaction } from './components/no-transaction/no-transaction';
-import { TransactionsService } from '../../shared/transaction/services/transactions';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { TransactionsService } from '../../shared/transaction/services/transactions.service';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, TransactionItem, NoTransaction],
+  imports: [
+    Balance,
+    TransactionItem,
+    NoTransaction,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
@@ -19,7 +27,6 @@ export class Home implements OnInit {
   ngOnInit(): void {
    this.getAllTransactions();
   }
-  
 
   private getAllTransactions() {
     this._transactionsService.getAll().subscribe({
