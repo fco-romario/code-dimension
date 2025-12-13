2623e4384f97c16ce34afd972abcc2d91c21fea3
fco-romario <fco.romario.alves@gmail.com>
2025-12-12 21:41:52 -0300

    feat: adiciona botão logout no header

--- gerenciador-financeiro/src/app/core/layout/components/header/header.ts
+++ gerenciador-financeiro/src/app/core/layout/components/header/header.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { LogoutFacadeService } from '../../../auth/facades/logout-facade.service';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { LoggedInUserStoreService } from '../../../auth/stores/logged-in-user-store.service';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  private _logoutFacadeService = inject(LogoutFacadeService);
+  private _router = inject(Router);
+  private _loggedInUserStoreService = inject(LoggedInUserStoreService);
 
+  isLoggedIn =  computed(() => this._loggedInUserStoreService.isLoggedIn());
+
+  logout() {
+    this._logoutFacadeService.logout()
+      .subscribe({
+        next: () => this._router.navigate(['./auth/login']),
+      })
+  }
 }
