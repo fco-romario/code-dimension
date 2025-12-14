5f92c01155bea5f1e9004032dce922bfefee1ada
fco-romario <fco.romario.alves@gmail.com>
2025-12-14 16:07:53 -0300

    feat: adiciona chips

--- gerenciador-financeiro/src/app/features/home/pages/list/components/transaction-item/transaction-item.ts
+++ gerenciador-financeiro/src/app/features/home/pages/list/components/transaction-item/transaction-item.ts
@@ -5,10 +5,11 @@ import { Transaction } from '@shared/transaction/interfaces/transaction';
 import { TransactionValue } from './components/transaction-value';
 import { CustomColorDirective } from '@shared/material/buttons/directives/custom-color.directive';
 import { IsIncomeDirective } from './directives/is-income.directive';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-transaction-item',
-  imports: [MatCardModule, MatButtonModule, TransactionValue, CustomColorDirective, IsIncomeDirective],
+  imports: [MatCardModule, MatButtonModule, TransactionValue, CustomColorDirective, IsIncomeDirective, MatChipsModule],
   templateUrl: './transaction-item.html',
   styleUrl: './transaction-item.scss',
 })
