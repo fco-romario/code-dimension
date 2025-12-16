f8ddf71b1e9b95b7f78c209ba3579da49114a8f9
fco-romario <fco.romario.alves@gmail.com>
2025-12-15 22:31:07 -0300

    feat: Configura pipe currency gloval

--- gerenciador-financeiro/src/app/core/provide-core.ts
+++ gerenciador-financeiro/src/app/core/provide-core.ts
@@ -1,9 +1,13 @@
-import { makeEnvironmentProviders } from "@angular/core";
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, makeEnvironmentProviders } from "@angular/core";
 import { provideAuth } from "./auth/provide-auth";
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarConfig } from "@angular/material/snack-bar";
 import { provideEnvironmentNgxMask } from "ngx-mask";
 import { setOuthTokenInterceptor } from "./auth/interceptors/set-outh-token-interceptor";
+import { registerLocaleData } from "@angular/common";
+import ptBr from "@angular/common/locales/pt";
+
+registerLocaleData(ptBr);
 
 export function provideCore() {
     return makeEnvironmentProviders([
@@ -22,5 +26,7 @@ export function provideCore() {
                 duration: 3000
             } as MatSnackBarConfig
         },
+        {   provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+        {   provide: LOCALE_ID, useValue: 'pt-BR' },
     ]);
 }
