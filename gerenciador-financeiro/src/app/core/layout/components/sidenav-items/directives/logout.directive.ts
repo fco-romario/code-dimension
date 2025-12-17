087e4caa9a91b3ea2b056ec7eb29ace393b627f0
fco-romario <fco.romario.alves@gmail.com>
2025-12-16 22:53:22 -0300

    refactor: move botão de logout para uma diretiva de atributo

---
+++ gerenciador-financeiro/src/app/core/layout/components/sidenav-items/directives/logout.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { LogoutFacadeService } from '../../../../auth/facades/logout-facade.service';
+
+@Directive({
+  selector: '[appLogout]',
+  host: {
+    '(click)': 'logout()'
+  }
+})
+export class LogoutDirective {
+  private _logoutFacadeService = inject(LogoutFacadeService);
+  private _router = inject(Router);
+
+  logout() {
+    this._logoutFacadeService.logout()
+      .subscribe({
+        next: () => this._router.navigate(['./auth/login']),
+    })
+  }
+
+}
--- gerenciador-financeiro/src/app/core/layout/components/sidenav-items/sidenav-items.component.ts
+++ gerenciador-financeiro/src/app/core/layout/components/sidenav-items/sidenav-items.component.ts
@@ -3,16 +3,15 @@ import { MatListModule } from '@angular/material/list';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { LogoutFacadeService } from '../../../auth/facades/logout-facade.service';
 import { LoggedInUserStoreService } from '../../../auth/stores/logged-in-user-store.service';
+import { LogoutDirective } from './directives/logout.directive';
 
 @Component({
   selector: 'app-sidenav-items',
-  imports: [MatListModule, RouterLink, RouterLinkActive],
+  imports: [MatListModule, RouterLink, RouterLinkActive, LogoutDirective],
   templateUrl: './sidenav-items.component.html',
   styleUrl: './sidenav-items.component.scss',
 })
 export class SidenavItemsComponent {
-  private _logoutFacadeService = inject(LogoutFacadeService);
-  private _router = inject(Router);
   private _loggedInUserStoreService = inject(LoggedInUserStoreService);
   
   isLoggedIn =  computed(() => this._loggedInUserStoreService.isLoggedIn());
@@ -24,10 +23,5 @@ export class SidenavItemsComponent {
     },
   ]);
 
-  logout() {
-    this._logoutFacadeService.logout()
-      .subscribe({
-        next: () => this._router.navigate(['./auth/login']),
-      })
-  }
+
 }
